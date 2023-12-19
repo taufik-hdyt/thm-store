7f6fc4ba2dc50f430f3517659895a751369632cf
taufik-hdyt <taufikhdyt2599@gmail.com>
2023-12-19 11:21:44 +0700

    updated

--- front-end/src/components/NavbarMobile/NavbarMobile.tsx
+++ front-end/src/components/NavbarMobile/NavbarMobile.tsx
@@ -1,16 +1,28 @@
-import { HStack, Link, Stack, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
-import { FaUserCircle } from "react-icons/fa";
-import { FaRegCircleUser } from "react-icons/fa6";
 import {
-  IoHomeOutline,
-  IoCartOutline,
-  IoCart,
-  IoHomeSharp,
-} from "react-icons/io5";
-import { MdLibraryMusic, MdMusicNote } from "react-icons/md";
+  HStack,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React from "react";
+import { FaUser } from "react-icons/fa";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { IoCartOutline, IoHomeSharp } from "react-icons/io5";
+import { MdLibraryMusic } from "react-icons/md";
+import { RiLogoutCircleFill } from "react-icons/ri";
+import { destroyCookie } from "nookies";
 
 const NavbarMobile: React.FC = (): JSX.Element => {
+  const router = useRouter();
+  function handleLogout() {
+    destroyCookie(null, "token");
+    router.push("/login");
+  }
 
   return (
     <HStack pos="fixed" bottom={0} p={1} roundedTop="xl" bg="primary" w="full">
@@ -42,15 +54,26 @@ const NavbarMobile: React.FC = (): JSX.Element => {
             </Text>
           </Stack>
         </Link>
-        <Link href="/profile">
-          <Stack align="center" spacing={1}>
-            <FaUserCircle size={24} color="white" />
 
-            <Text fontSize="xs" color="white  " fontWeight="semibold">
+        <Menu>
+          <MenuButton>
+            <Stack spacing={1} align="center">
+              <FaRegCircleUser color="white" size={24} />
+              <Text fontSize="xs" color="white  " fontWeight="semibold">
+                Profile
+              </Text>
+            </Stack>
+          </MenuButton>
+
+          <MenuList>
+            <MenuItem onClick={() => router.push("/profile")} icon={<FaUser />}>
               Profile
-            </Text>
-          </Stack>
-        </Link>
+            </MenuItem>
+            <MenuItem onClick={handleLogout} icon={<RiLogoutCircleFill />}>
+              Logout
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </HStack>
     </HStack>
   );
