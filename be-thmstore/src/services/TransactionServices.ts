93756d274f459584e16f7911194c6a6116f32abf
taufik-hdyt <taufikhdyt2599@gmail.com>
2023-12-26 16:50:27 +0700

    updated

---
+++ be-thmstore/src/services/TransactionServices.ts
@@ -0,0 +1,31 @@
+import { Repository } from "typeorm";
+import { Transaction } from "../entity/Transaction";
+import { AppDataSource } from "../data-source";
+import { Request, Response } from "express";
+import { Customer } from "../entity/Customer";
+import { Product } from "../entity/Product";
+
+export default new (class TransactionServices {
+  private readonly TransactionRepository: Repository<Transaction> =
+    AppDataSource.getRepository(Transaction);
+    private readonly CustomerRepository: Repository<Customer> =
+    AppDataSource.getRepository(Customer);
+    private readonly ProductRepository: Repository<Product> =
+    AppDataSource.getRepository(Product);
+
+//   async addTransaction(req: Request, res: Response): Promise<Response> {
+//     try {
+
+//         // const customer = await this.CustomerRepository.findOne({
+//         //     where: {
+//         //         customer_id: 
+//         //     }
+//         // })
+
+//     } catch (error) {
+//       return res.status(500).json({
+//         message: error.message,
+//       });
+//     }
+//   }
+// })();
