f9aa999abae12e23baebd46db45ecdbb4e452148
taufik-hdyt <taufikhdyt2599@gmail.com>
2023-12-31 01:09:09 +0700

    updated

--- front-end/src/containers/Profile/partials/Transaction.tsx
+++ front-end/src/containers/Profile/partials/Transaction.tsx
@@ -1,3 +1,7 @@
+import { useAuth } from "@/hooks/useAuth";
+import { ITransaction } from "@/interface/transaction.interfaces";
+import { formatRupiah } from "@/utils/formatRupiah";
+
 import {
   Box,
   Button,
@@ -9,6 +13,7 @@ import {
   Flex,
 } from "@chakra-ui/react";
 import { MdOutlineShoppingBag } from "react-icons/md";
+import moment from 'moment'
 
 const Transaction: React.FC = (): JSX.Element => {
   const customStyleTitle: React.CSSProperties = {
@@ -17,6 +22,8 @@ const Transaction: React.FC = (): JSX.Element => {
     WebkitBoxOrient: "vertical",
     overflow: "hidden",
   };
+
+  const {user} = useAuth()
 
   return (
     <Box>
@@ -33,51 +40,59 @@ const Transaction: React.FC = (): JSX.Element => {
       </HStack>
       
 
-      <Stack mt={6}>
-        <Card p={4}>
-          <HStack spacing={4}>
-            <MdOutlineShoppingBag size={24} />
-            <Text fontSize="xs">10 Sep 2022</Text>
-            <Text
-              fontSize="sm"
-              px="3"
-              border="1px solid green"
-              rounded="xl"
-              color="green"
-              fontWeight="semibold"
-            >
-              Done
-            </Text>
-            <Text fontSize="sm">INV/20220910</Text>
-          </HStack>
-
-          <HStack mt="2">
-            <Image
-              w="100px"
-              h="100px"
-              src="https://id.yamaha.com/id/files/Image-Index_F-FX_1080x1080_3b098c9164d20654e054d375fd2ccba4.jpg?impolicy=resize&imwid=396&imhei=396"
-              alt="name"
-            />
-            <Stack spacing={0} w="full">
-              <Text fontWeight="semibold" style={customStyleTitle}>
-                Gitar Yamaha G-139 Original 
+      <Stack mt={6} spacing={4}>
+      {
+        user?.transactions.map((data:ITransaction,idx:number)=> {
+          const waktuMoment = moment(data.transaction_date, "HH:mm:ss.SSSSSSZ");
+          const transactionDate = waktuMoment.format("YYYY-MM-DD");
+          return (
+            <Card key={idx} p={4}>
+            <HStack spacing={4}>
+              <MdOutlineShoppingBag size={24} />
+              <Text fontSize="xs">{transactionDate}</Text>
+              <Text
+                fontSize="sm"
+                px="3"
+                border="1px solid green"
+                rounded="xl"
+                color="green"
+                fontWeight="semibold"
+              >
+                {data.status_payment}
               </Text>
-              <HStack justify="space-between" >
-              <Text fontSize="xs">Qty 1 x Rp 1.200.000</Text>
-              <Stack spacing={0}>
-                <Box display={{base: "none", md: "block"}}>
-              <Text  fontWeight="semibold">Total</Text>  
-              <Text fontWeight="semibold">Rp 1.200.000</Text>
-                </Box>
+              <Text fontSize="sm">{data.no_transaction}</Text>
+            </HStack>
+  
+            <HStack mt="2">
+              <Image
+                w="100px"
+                h="100px"
+                src={data.product.image}
+                alt={data.product.product_name}
+              />
+              <Stack spacing={0} w="full">
+                <Text fontWeight="semibold" style={customStyleTitle}>
+                  {data.product.product_name}
+                </Text>
+                <HStack justify="space-between" >
+                <Text fontSize="xs">Qty {data.quantity} x {formatRupiah(data.product.price)}</Text>
+                <Stack spacing={0}>
+                  <Box display={{base: "none", md: "block"}}>
+                <Text  fontWeight="semibold">Total</Text>  
+                <Text fontWeight="semibold">{formatRupiah(data.subtotal)}</Text>
+                  </Box>
+                </Stack>
+                </HStack>
+                <Box display={{base: "block", md: "none"}}>
+                <Text  fontWeight="semibold">Total</Text>  
+                <Text fontWeight="semibold">{formatRupiah(data.subtotal)}</Text>
+                  </Box>
               </Stack>
-              </HStack>
-              <Box display={{base: "block", md: "none"}}>
-              <Text  fontWeight="semibold">Total</Text>  
-              <Text fontWeight="semibold">Rp 1.200.000</Text>
-                </Box>
-            </Stack>
-          </HStack>
-        </Card>
+            </HStack>
+          </Card>
+          )
+        })
+      }
       </Stack>
     </Box>
   );
--- front-end/src/interface/transaction.interfaces.ts
+++ front-end/src/interface/transaction.interfaces.ts
@@ -1,13 +1,25 @@
+import { IProducts } from "./product.interface";
+
 export interface ICreateTransaction {
   status_payment: "SUCCESS" | "PENDING" | "CANCELED";
   status_pengiriman: "ON PROCCESS" | "ON DELIVERED" | "DELIVERED";
   quantity: number;
   subtotal: number;
-  customer: number
-  product: number 
-  product_name: string
-  price: number
-  product_id: number
+  customer: number;
+  product: number;
+  product_name: string;
+  price: number;
+  product_id: number;
 }
 
-
+export interface ITransaction {
+  transaction_id: number
+  no_transaction: number
+  customer: number
+  transaction_date: string
+  status_payment: string
+  status_pengiriman: string
+  quantity: number
+  subtotal: number
+  product: IProducts
+}
