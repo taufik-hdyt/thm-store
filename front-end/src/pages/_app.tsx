fecc503d56d08f045e6af51b83bbccf5c32b5b66
taufik-hdyt <taufikhdyt2599@gmail.com>
2024-01-01 15:02:39 +0700

    updated

--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, HStack, Text } from "@chakra-ui/react";
 import { myNewTheme } from "@/styles/theme";
 import nookies from 'nookies'
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "@/providers/AuthProvider";
+import { useAuth } from "@/hooks/useAuth";
+import { Loading } from "@/components/LoadingAnimation/loadingAnimation";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,13 +19,21 @@ const queryClient = new QueryClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   const { token } = nookies.get();
+  
   return (
     <AuthProvider token={token}>
       <QueryClientProvider client={queryClient}>
         <ChakraProvider theme={myNewTheme}>
-          <Component {...pageProps} />
+         <Component {...pageProps} />
         </ChakraProvider>
       </QueryClientProvider>
     </AuthProvider>
   );
 }
+
+{/* <Box display="flex" justifyContent="center" alignContent="center" h="100vh">
+              <HStack>
+              <Loading />
+              <Text>Loading...</Text>
+              </HStack>
+            </Box> */}
