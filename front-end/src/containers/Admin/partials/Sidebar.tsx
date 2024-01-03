6c366b76b39f7bc067f406500d4a59793eb762a9
taufik-hdyt <taufikhdyt2599@gmail.com>
2024-01-03 17:15:27 +0700

    updated

--- front-end/src/containers/Admin/partials/Sidebar.tsx
+++ front-end/src/containers/Admin/partials/Sidebar.tsx
@@ -3,12 +3,22 @@ import { LuShoppingBag } from "react-icons/lu";
 import { FaUsers } from "react-icons/fa";
 import { BsCart3 } from "react-icons/bs";
 import { SiBrandfolder } from "react-icons/si";
+import { IoMdLogOut } from "react-icons/io";
+import {destroyCookie} from 'nookies'
+import { useRouter } from "next/router";
 
 interface IProps{
     setState: (I:string)=> void
     selected: string
 }
 const Sidebar: React.FC<IProps> = ({setState,selected}): JSX.Element => {
+  const router = useRouter()
+
+  function handleLogout() {
+    destroyCookie(null, "token");
+    router.push("/login");
+  }
+
   return (
     <Box p={6} display="flex" justifyContent="center">
       <Stack w="full">
@@ -68,6 +78,18 @@ const Sidebar: React.FC<IProps> = ({setState,selected}): JSX.Element => {
             bg={selected === "users" ? "#ebebeb" : "transparent"}
           >
             Users
+          </Button>
+
+          <Button
+          px={3}
+            variant="unstyled"
+            display="flex"
+            justifyContent="left"
+            gap={4}
+            leftIcon={<IoMdLogOut  size={24} />}
+            onClick={handleLogout}
+          >
+            Logout
           </Button>
         </Stack>
       </Stack>
