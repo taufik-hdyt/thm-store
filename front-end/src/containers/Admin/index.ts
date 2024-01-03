978ee15ad73b6d5bb44ff5dbfb5ea03e6ca10e53
taufik-hdyt <taufikhdyt2599@gmail.com>
2024-01-03 09:04:14 +0700

    updated

--- front-end/src/containers/Admin/index.ts
+++ front-end/src/containers/Admin/index.ts
@@ -1,4 +1,4 @@
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
 
-const Admin = dynamic(import("./Admin"));
+const Admin = dynamic(import('./Admin'), { ssr: false });
 export default Admin;
--- front-end/src/pages/admin/index.tsx
+++ front-end/src/pages/admin/index.tsx
@@ -3,40 +3,37 @@ import Admin from "@/containers/Admin";
 import { useAuth } from "@/hooks/useAuth";
 import { NextPage, NextPageContext } from "next";
 import { useRouter } from "next/router";
-import nookies from "nookies"
+import nookies from "nookies";
 import { useEffect } from "react";
 
 const AdminPage: NextPage = (): JSX.Element => {
-  const {user} = useAuth()
-  const router = useRouter()
- 
+  const { user } = useAuth();
+  const router = useRouter();
 
-  useEffect(()=>{
-    if(user?.email !== "admin@gmail.com"){
-      router.push("/")
+  useEffect(() => {
+    if (user?.email !== "admin@gmail.com") {
+      router.push("/");
     }
-  },[user])
+  }, [user]);
 
-  return (
-      <Admin />
-  );
+  return <Admin />;
 };
 
-export async function getServerSideProps(context:NextPageContext){
-  const cookies = nookies.get(context)
-  if(!cookies.token){
+export async function getServerSideProps(context: NextPageContext) {
+  const cookies = nookies.get(context);
+  if (!cookies.token) {
     return {
       redirect: {
-        destination: "/login"
-      }
-    }
+        destination: "/login",
+      },
+    };
   }
 
   return {
     props: {
-      title: "Admin"
-    }
-  }
+      title: "Admin",
+    },
+  };
 }
 
 export default AdminPage;
